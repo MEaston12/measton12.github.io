c38db4314a9041cff390e72adba246f312b2cfa8
MEaston12 <eastonmalcolm@gmail.com>
2021-08-22 18:20:43 -0600

    footer starter

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,13 @@
-const Footer = () => (
-  <div></div>
-);
+import Container from "react-bootstrap/Container";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const Footer = () => (<>
+  <div style={{'height':'41px'}}></div>
+  <Navbar fixed='bottom' bg='light'>
+    <Container>
+      <h3>Media</h3>
+    </Container>
+  </Navbar>
+</>);
 export default Footer;
