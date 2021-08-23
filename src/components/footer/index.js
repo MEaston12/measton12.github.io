e33b5c97146fa1f945226984dcf969ff00e852a2
MEaston12 <eastonmalcolm@gmail.com>
2021-08-22 18:27:19 -0600

    added links to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,13 @@
 import Container from "react-bootstrap/Container";
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
 const Footer = () => (<>
   <div style={{'height':'41px'}}></div>
   <Navbar fixed='bottom' bg='light'>
     <Container>
-      <h3>Media</h3>
+      <h4>Github <a target="_blank" href='https://github.com/MEaston12'>MEaston12</a></h4>
+      <h4><a target="_blank" href='https://www.linkedin.com/in/malcolm-easton-67377896/'>My LinkedIn</a></h4>
+      <h4><a target="_blank" href='https://stackoverflow.com/users/15311882/malcolm-easton'>My StackOverflow</a></h4>
     </Container>
   </Navbar>
 </>);
