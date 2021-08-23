ec03bea5f7f6db0e32506016a62156329fb1e431
MEaston12 <eastonmalcolm@gmail.com>
2021-08-22 20:10:52 -0600

    fixed resume preview

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,7 +5,7 @@ const Resume = () => (
   <Container className='p-4'>
     <h1>Resume</h1>
     <a href='http://docs.google.com/document/d/1DjaJCq0QPXuZstYunS2P0mB_ZMpU896jNp_CKAcg1_g/export?format=pdf'>Download PDF</a>
-    <GoogleDocsViewer width='100%' height='75vh' fileUrl='http://docs.google.com/document/d/1DjaJCq0QPXuZstYunS2P0mB_ZMpU896jNp_CKAcg1_g/export?format=pdf'/>
+    <GoogleDocsViewer width='100%' height='70vh' fileUrl='http://docs.google.com/document/d/1DjaJCq0QPXuZstYunS2P0mB_ZMpU896jNp_CKAcg1_g/export?format=pdf'/>
   </Container>
 );
 export default Resume;
