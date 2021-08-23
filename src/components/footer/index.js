d9bd3c64d6b5d431027326be09226004679f6e43
MEaston12 <eastonmalcolm@gmail.com>
2021-08-22 20:10:44 -0600

    cleanedup footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => (<>
   <div style={{'height':'41px'}}></div>
   <Navbar fixed='bottom' bg='light'>
     <Container>
-      <h4>Github <a target="_blank" href='https://github.com/MEaston12'>MEaston12</a></h4>
-      <h4><a target="_blank" href='https://www.linkedin.com/in/malcolm-easton-67377896/'>My LinkedIn</a></h4>
-      <h4><a target="_blank" href='https://stackoverflow.com/users/15311882/malcolm-easton'>My StackOverflow</a></h4>
+      <h4><a target="_blank" href='https://github.com/MEaston12'>Github</a></h4>
+      <h4><a target="_blank" href='https://www.linkedin.com/in/malcolm-easton-67377896/'>LinkedIn</a></h4>
+      <h4><a target="_blank" href='https://stackoverflow.com/users/15311882/malcolm-easton'>StackOverflow</a></h4>
     </Container>
   </Navbar>
 </>);
