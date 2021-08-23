d03496f2f98990aa7083bd9c91ec89be175db949
MEaston12 <eastonmalcolm@gmail.com>
2021-08-22 18:20:27 -0600

    fixed nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,9 +2,15 @@ import Bav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
 
 const Nav = () => {
-  const path = window.location.pathname;
+  const [activePath, changeActivePath] = useState('/');
+
+  const updateActivePath = event => {
+    changeActivePath(event.target.id);
+  }
+
   return (
     <Navbar sticky="top" bg="dark" variant='dark' expand="lg">
       <Container>
@@ -12,10 +18,10 @@ const Nav = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Bav className="me-auto">
-            <Bav.Link as={Link} to="/about" className={(path === '/#/about' || path === '/' || path === '/#/') && 'active'}>About</Bav.Link>
-            <Bav.Link as={Link} to="/contact" className={path === '/#/contact' && 'active'}>Contact</Bav.Link>
-            <Bav.Link as={Link} to="/portfolio" className={path === '/#/portfolio' && 'active'}>Portfolio</Bav.Link>
-            <Bav.Link as={Link} to="/resume" className={path === '/#/resume' && 'active'}>Resume</Bav.Link>
+            <Bav.Link as={Link} to="/about" id='about' onClick={updateActivePath} className={(activePath === 'about' || activePath === '/') && 'active'}>About</Bav.Link>
+            <Bav.Link as={Link} to="/contact" id='contact' onClick={updateActivePath} className={activePath === 'contact' && 'active'}>Contact</Bav.Link>
+            <Bav.Link as={Link} to="/portfolio" id='portfolio' onClick={updateActivePath} className={activePath === 'portfolio' && 'active'}>Portfolio</Bav.Link>
+            <Bav.Link as={Link} to="/resume" id='resume' onClick={updateActivePath} className={activePath === 'resume' && 'active'}>Resume</Bav.Link>
           </Bav>
         </Navbar.Collapse>
       </Container>
