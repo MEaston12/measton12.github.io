e4dea7be962769b79c6507a078a9d33345673b16
MEaston12 <eastonmalcolm@gmail.com>
2021-08-27 01:42:03 -0600

    security fix

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => (<>
   <div style={{'height':'41px'}}></div>
   <Navbar fixed='bottom' bg='dark'>
     <Container>
-      <h4><a target="_blank" href='https://github.com/MEaston12'>Github</a></h4>
-      <h4><a target="_blank" href='https://www.linkedin.com/in/malcolm-easton-67377896/'>LinkedIn</a></h4>
-      <h4><a target="_blank" href='https://stackoverflow.com/users/15311882/malcolm-easton'>StackOverflow</a></h4>
+      <h4><a target="_blank" rel='noreferrer' href='https://github.com/MEaston12'>Github</a></h4>
+      <h4><a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/malcolm-easton-67377896/'>LinkedIn</a></h4>
+      <h4><a target="_blank" rel='noreferrer' href='https://stackoverflow.com/users/15311882/malcolm-easton'>StackOverflow</a></h4>
     </Container>
   </Navbar>
 </>);
